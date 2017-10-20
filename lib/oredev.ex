29d7361537d11bc7ca65315ed4630f353adea553
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-10-20 13:38:49 +0100

    First commit

---
+++ lib/oredev.ex
@@ -0,0 +1,18 @@
+defmodule Oredev do
+  @moduledoc """
+  Documentation for Oredev.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Oredev.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/oredev_test.exs
@@ -0,0 +1,8 @@
+defmodule OredevTest do
+  use ExUnit.Case
+  doctest Oredev
+
+  test "greets the world" do
+    assert Oredev.hello() == :world
+  end
+end
