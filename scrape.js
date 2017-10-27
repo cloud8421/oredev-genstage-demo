2d2982c9a50f71a76311e78ed1270cec85d6113f
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2017-10-27 14:48:40 +0100

    Adds topics to schedule

--- scrape.js
+++ scrape.js
@@ -4,9 +4,14 @@ var data = $('.session-day')
 
     return $('.session-slot', day)
       .map(function(j, s) {
+        var topics = s.className.split(" ")
+          .filter(function(a) { return a.startsWith("Tag") })
+          .map(function(topic) { return topic.replace("Tag", "") });
+
         return {speaker: $('.speaker', s).text(),
                 day: dayNumber,
                 title: $('.title', s).text(),
+                topics: topics,
                 room: $('.room', s).text(),
                 starts_at: $('.time', s).text()}
       }).toArray();
